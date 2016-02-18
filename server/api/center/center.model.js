7d513a00b45bb59083e7d1fc4de85c005e9fd1e7
Eli <elibo@live.com>
2016-02-18 11:22:59 +0100

    centerupdated

--- server/api/center/center.model.js
+++ server/api/center/center.model.js
@@ -3,8 +3,13 @@
 var mongoose = require('bluebird').promisifyAll(require('mongoose'));
 
 var CenterSchema = new mongoose.Schema({
-  name: String,
-  
+  name: {type:String, required:true},
+  address : String,
+  code: String,
+  users:[{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+  copies:[{type: mongoose.Schema.Types.ObjectId, ref: 'Copy'}],
+  borrowers: [{type: mongoose.Schema.Types.ObjectId, ref: 'Borrower'}],
+  locations :[{type: mongoose.Schema.Types.ObjectId, ref: 'Location'}]
 });
 
 export default mongoose.model('Center', CenterSchema);
