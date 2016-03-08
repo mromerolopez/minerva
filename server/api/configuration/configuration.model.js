5b6e6ca280e9bd001b2ec4e48f48cd51311f5254
Miguel <miky.rl@gmail.com>
2016-03-08 11:09:24 +0100

    model updated

--- server/api/configuration/configuration.model.js
+++ server/api/configuration/configuration.model.js
@@ -13,12 +13,11 @@ var ConfigurationSchema = new mongoose.Schema({
             created_time: {type: Date, default: Date.now}
         }
     ],
-    schoolar_year: {
-        start_date: Date,
-        finish_date: Date
-    },
+   
     //default_borrow_time: {type: Number, default: 15},
-    borrower_types: [{type:String, default_loan_time: Number, default_loan_end: Date}]
+    borrower_types: [{type:String, default_loan_time: Number, default_loan_end: Date, active: { type: Boolean, default:true }}],
+    book_type: [{name:String, active: { type: Boolean, default:true }}],
+    locations: [{name:String, active: { type: Boolean, default:true }}]
 });
 
 export default mongoose.model('Configuration', ConfigurationSchema);
